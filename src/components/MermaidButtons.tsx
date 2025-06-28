cfa74d887708e4f30e9e149fa3dacf600a396f98
boop9 <85202743+boop9@users.noreply.github.com>
2025-06-28 18:53:01 +0500

    first commit

---
+++ src/components/MermaidButtons.tsx
@@ -0,0 +1,11 @@
+const MermaidButtons = () => {
+  return (
+    <>
+      <div className="max-lg:">
+        <img src="public/rectangle.svg" alt="rectangle" /> <div>square</div>
+      </div>
+    </>
+  );
+};
+
+export default MermaidButtons;
